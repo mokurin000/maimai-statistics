a5864a8c79778a9c9e9e7968e7d109f9e54403f9
mokurin000 <1348292515a@gmail.com>
2025-08-04 12:17:00 +0800

    feat: add region related charts

---
+++ pc_rating_scatter.py
@@ -0,0 +1,65 @@
+from typing import Literal
+
+import polars as pl
+import pyecharts.options as opts
+from pyecharts.charts import Scatter
+
+
+pc = (
+    pl.scan_parquet("regions.parquet")
+    .drop(["region_id", "created"])
+    .group_by("user_id")
+    .sum()
+)
+
+rating = (
+    pl.scan_parquet("players.parquet")
+    .rename({"userId": "user_id", "playerRating": "rating"})
+    .select(["user_id", "rating"])
+    .join(pc, on="user_id")
+    .drop("user_id")
+    .collect()
+)
+
+
+x_data = rating["play_count"]
+y_data = rating["rating"]
+
+
+def init_chart(
+    x_type: Literal["value", "log"], x_min: int = 1, x_max: int = 5000
+) -> Scatter:
+    return (
+        Scatter(
+            init_opts=opts.InitOpts(
+                width="1600px",  # 设置图表宽度
+                height="1000px",  # 设置图表高度
+            )
+        )
+        .set_series_opts()
+        .set_global_opts(
+            xaxis_opts=opts.AxisOpts(
+                type_=x_type,
+                min_=x_min,
+                max_=x_max,
+            ),
+            yaxis_opts=opts.AxisOpts(
+                type_="value",
+                axistick_opts=opts.AxisTickOpts(is_show=True),
+                splitline_opts=opts.SplitLineOpts(is_show=True),
+            ),
+            tooltip_opts=opts.TooltipOpts(is_show=False),
+            visualmap_opts=opts.VisualMapOpts(max_=16400),
+        )
+        .add_xaxis(xaxis_data=x_data.to_list())
+        .add_yaxis(
+            series_name="",
+            y_axis=y_data.to_list(),
+            symbol_size=2.5,
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+    )
+
+
+init_chart("value", 1).render("pc-rating-linear.html")
+init_chart("log", 10, 10000).render("pc-rating-log.html")
---
+++ region_count.py
@@ -0,0 +1,87 @@
+import polars as pl
+
+from pyecharts import options as opts
+from pyecharts.charts import Map
+
+
+def region_name(region_id: int) -> str:
+    name = {
+        1: "北京",
+        2: "重庆",
+        3: "上海",
+        4: "天津",
+        5: "安徽",
+        6: "福建",
+        7: "甘肃",
+        8: "广东",
+        9: "贵州",
+        10: "海南",
+        11: "河北",
+        12: "黑龙江",
+        13: "河南",
+        14: "湖北",
+        15: "湖南",
+        16: "江苏",
+        17: "江西",
+        18: "吉林",
+        19: "辽宁",
+        20: "青海",
+        21: "陕西",
+        22: "山东",
+        23: "山西",
+        24: "四川",
+        26: "云南",
+        27: "浙江",
+        28: "广西",
+        29: "内蒙古",
+        30: "宁夏",
+        31: "新疆",
+        32: "西藏",
+    }.get(region_id, "未知")
+
+    special = {
+        "内蒙古": "自治区",
+        "西藏": "自治区",
+        "宁夏": "回族自治区",
+        "广西": "壮族自治区",
+        "新疆": "维吾尔自治区",
+    }
+
+    if name == "未知":
+        return name
+
+    if name in special:
+        return f"{name}{special[name]}"
+    elif name in ["北京", "上海", "重庆", "天津"]:
+        return f"{name}市"
+    else:
+        return f"{name}省"
+
+
+df = (
+    pl.scan_parquet("regions.parquet")
+    .sort("play_count", descending=True)
+    .unique(subset=["user_id"], keep="first")
+    .with_columns(pl.col("region_id").map_elements(region_name, return_dtype=pl.String))
+    .group_by("region_id")
+    .len()
+    .sort("len", descending=True)
+    .collect()
+)
+
+c = (
+    Map(
+        init_opts=opts.InitOpts(
+            width="1600px",  # 设置图表宽度
+            height="1000px",  # 设置图表高度
+        )
+    )
+    .add("", list(df.iter_rows()), "china")
+    .set_global_opts(
+        title_opts=opts.TitleOpts(title="舞萌玩家地区分布"),
+        visualmap_opts=opts.VisualMapOpts(
+            max_=80000,
+        ),
+    )
+    .render("maimai-region.html")
+)
